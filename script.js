85d761d8cba0f29f4ad8107e11dded9ab31b6f75
Jacob Dominic <>
2021-05-14 13:46:35 +0000

    123456

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById('area').addEventListener('click', areaButton)
 document.getElementById('circumference').addEventListener('click', circumferenceButton)
 document.getElementById('area1').addEventListener('click', area1Button)
+document.getElementById('area2').addEventListener('click', area2Button)
 // Connects buttons to area and circumference
 
 let radius = 0
@@ -8,6 +9,8 @@ let area = 0
 let circumference = 0
 let pinumber = Math.PI
 let side = 0
+let base = 0
+let height = 0
 // Sets Variables/Let statements
 
 function areaButton () {
@@ -34,4 +37,14 @@ function area1Button () {
 
   area = side * side
   document.getElementById('area1-answer').innerHTML = area
-}
+}// Function of area1Button
+
+function area2Button () {
+  base = document.getElementById('trianglebase').value
+  base = parseFloat(base)
+  height = document.getElementById('triangleheight').value
+  height = parseFloat(height)
+
+  area = (base * height)/2
+  document.getElementById('area2-answer').innerHTML = area
+}// Function of area2Button
